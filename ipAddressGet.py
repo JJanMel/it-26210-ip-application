ba3cdf2050ddff07ab8fa55a8151668cb165d615
JJanMel <jjanmelvin@gmail.com>
2021-10-12 20:29:22 +0800

    add_file

---
+++ ipAddressGet.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+import requests
+
+#Create the window that will display the application
+sample = tk.Tk()
+sample.title('IP Getter')
+sample.geometry("250x100")
+
+#Function to set the text of the input
+def setTextInput(text):
+    textExample.delete(0,"end")
+    textExample.insert(0, text)
+
+textExample = tk.Entry(sample)
+textExample.pack()
+
+#Button for the application to begin its process
+button = tk.Button(sample, text='Get IP', width=10, command = lambda:setTextInput(requests.get('https://api.ipify.org').text))
+button.place(x= 90, y = 30)
+
+sample.mainloop()
+
+
