c850db68b6726954af142f950a09d405a678ef43
JJanMel <jjanmelvin@gmail.com>
2021-10-12 20:38:24 +0800

    comments update

--- ipAddressGet.py
+++ ipAddressGet.py
@@ -6,11 +6,12 @@ sample = tk.Tk()
 sample.title('IP Getter')
 sample.geometry("250x100")
 
-#Function to set the text of the input
+#Function to set the text of the input box
 def setTextInput(text):
     textExample.delete(0,"end")
     textExample.insert(0, text)
 
+#Adds the input box into the GUI
 textExample = tk.Entry(sample)
 textExample.pack()
 
@@ -18,6 +19,7 @@ textExample.pack()
 button = tk.Button(sample, text='Get IP', width=10, command = lambda:setTextInput(requests.get('https://api.ipify.org').text))
 button.place(x= 90, y = 30)
 
+#Inititalize the GUI
 sample.mainloop()
 
 
