a3a52555502864043a71970d1f4f27e668ec705f
JJanMel <jjanmelvin@gmail.com>
2021-11-17 21:19:46 +0800

    using new api

--- ipAddressGet.py
+++ ipAddressGet.py
@@ -1,25 +1,8 @@
-import tkinter as tk
-import requests
-
-#Create the window that will display the application
-sample = tk.Tk()
-sample.title('IP Getter')
-sample.geometry("250x100")
-
-#Function to set the text of the input box
-def setTextInput(text):
-    textExample.delete(0,"end")
-    textExample.insert(0, text)
-
-#Adds the input box into the GUI
-textExample = tk.Entry(sample)
-textExample.pack()
-
-#Button for the application to begin its process
-button = tk.Button(sample, text='Get IP', width=10, command = lambda:setTextInput(requests.get('https://api.ipify.org').text))
-button.place(x= 90, y = 30)
-
-#Inititalize the GUI
-sample.mainloop()
+from requests import get
 
 
+#This variable is used to get the ip from the user's computer
+ip = get('https://ip.seeip.org/geoip').text
+
+#This is used to print the ip and other information
+print ('My public IPv4 address and a few more bits of information: {}'.format(ip))
